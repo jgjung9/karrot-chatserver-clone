fb63db25e6ee02b20ed2456ca6ffdae57e88aefa
gio <wnsgyo9@gmail.com>
2025-07-31 14:32:34 +0900

    Update id strategy

--- src/main/java/karrot/chat/chatserver/domain/account/entity/Account.java
+++ src/main/java/karrot/chat/chatserver/domain/account/entity/Account.java
@@ -2,6 +2,7 @@ package karrot.chat.chatserver.domain.account.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -9,7 +10,7 @@ import lombok.Data;
 @Data
 public class Account {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
