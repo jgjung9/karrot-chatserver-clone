9077b8bccff67a8b5bb691eee6eaf5d08ba9af0f
gio <wnsgyo9@gmail.com>
2025-07-31 23:27:45 +0900

    Add web socket config file

---
+++ src/main/java/karrot/chat/chatserver/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package karrot.chat.chatserver.config;
+
+import karrot.chat.chatserver.domain.chat.websocket.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .setAllowedOrigins("*");
+    }
+}
