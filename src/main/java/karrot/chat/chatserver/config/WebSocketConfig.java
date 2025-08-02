d374756728f86491b399525694e93132e5f368d9
gio <wnsgyo9@gmail.com>
2025-08-03 07:08:14 +0900

    Add user handshake interceptor

--- src/main/java/karrot/chat/chatserver/config/WebSocketConfig.java
+++ src/main/java/karrot/chat/chatserver/config/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package karrot.chat.chatserver.config;
 
 import karrot.chat.chatserver.domain.chat.websocket.ChatWebSocketHandler;
+import karrot.chat.chatserver.domain.chat.websocket.UserHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -13,10 +15,12 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final ChatWebSocketHandler chatWebSocketHandler;
+    private final UserHandshakeInterceptor userHandshakeInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .addInterceptors(userHandshakeInterceptor)
                 .setAllowedOrigins("*");
     }
 }
---
+++ src/main/java/karrot/chat/chatserver/domain/chat/websocket/UserHandshakeInterceptor.java
@@ -0,0 +1,37 @@
+package karrot.chat.chatserver.domain.chat.websocket;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.net.URI;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class UserHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest servletServerHttpRequest) {
+            HttpServletRequest httpRequest = servletServerHttpRequest.getServletRequest();
+            String userId = httpRequest.getParameter("userId");
+            log.debug("userId={}", userId);
+            if (userId == null || userId.isEmpty()) {
+                return false;
+            }
+            attributes.put("userId", userId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
