2c6ad9ca9aca33fc5d8eaa2c07974488fbb4f169
gio <wnsgyo9@gmail.com>
2025-08-14 18:30:24 +0900

    enable scheduling

--- src/main/java/karrot/chat/chatserver/ChatserverApplication.java
+++ src/main/java/karrot/chat/chatserver/ChatserverApplication.java
@@ -2,8 +2,10 @@ package karrot.chat.chatserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ChatserverApplication {
 
 	public static void main(String[] args) {
