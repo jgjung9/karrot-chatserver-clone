8c2b7ccd01e49feb7e178aa34591fb71477b9fdd
gio <wnsgyo9@gmail.com>
2025-08-14 18:32:46 +0900

    update chat service

--- src/main/java/karrot/chat/chatserver/domain/chat/service/ChatService.java
+++ src/main/java/karrot/chat/chatserver/domain/chat/service/ChatService.java
@@ -1,18 +1,20 @@
 package karrot.chat.chatserver.domain.chat.service;
 
 import karrot.chat.chatserver.domain.chat.command.CommandManager;
-import karrot.chat.chatserver.domain.chat.command.server.ServerCommands;
-import karrot.chat.chatserver.domain.chat.dto.payload.PushMessageRequest;
+import karrot.chat.chatserver.domain.chat.command.server.ServerInitiatedCommandType;
+import karrot.chat.chatserver.domain.chat.dto.payload.DeliverMessageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ChatService {
 
     private final CommandManager commandManager;
 
-    public void sendChat(PushMessageRequest request) {
-        commandManager.send(request.getUserId(), ServerCommands.PUSH_MESSAGE, request);
+    public void sendMessageToClient(DeliverMessageRequest request) {
+        commandManager.executeServerInitiated(request.getReceiverId(), ServerInitiatedCommandType.DELIVER_MESSAGE, request);
     }
 }
