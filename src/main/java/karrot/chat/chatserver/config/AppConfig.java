83e09e2886da49c467dc0724985810110789a7d8
gio <wnsgyo9@gmail.com>
2025-08-14 18:15:11 +0900

    Delete account part

--- src/main/java/karrot/chat/chatserver/config/AppConfig.java
+++ src/main/java/karrot/chat/chatserver/config/AppConfig.java
@@ -3,32 +3,14 @@ package karrot.chat.chatserver.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
-
-    @Bean
-    @Primary
-    public JPAQueryFactory accountQueryFactory(
-            @Qualifier("accountEntityManager") EntityManager accountEntityManager
-    ) {
-        return new JPAQueryFactory(accountEntityManager);
-    }
-
-    @Bean
-    public JPAQueryFactory chatQueryFactory(
-            @Qualifier("chatEntityManager") EntityManager chatEntityManager
-    ) {
-        return new JPAQueryFactory(chatEntityManager);
-    }
+    
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
--- src/main/java/karrot/chat/chatserver/config/ChatDataSourceConfig.java
+++ src/main/java/karrot/chat/chatserver/config/ChatDataSourceConfig.java
@@ -1,58 +1,15 @@
 package karrot.chat.chatserver.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-        basePackages = "karrot.chat.chatserver.domain.chat.repository",
-        entityManagerFactoryRef = "chatEntityManager",
-        transactionManagerRef = "chatTransactionManager"
-)
 public class ChatDataSourceConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.chat")
-    public DataSourceProperties chatDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    public DataSource chatDataSource() {
-        return chatDataSourceProperties()
-                .initializeDataSourceBuilder()
-                .build();
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean chatEntityManager(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("chatDataSource") DataSource dataSource
-            ) {
-        return builder
-                .dataSource(dataSource)
-                .packages("karrot.chat.chatserver.domain.chat.entity")
-                .persistenceUnit("chat")
-                .build();
-    }
-
-    @Bean
-    public PlatformTransactionManager chatTransactionManager(
-            @Qualifier("chatEntityManager")EntityManagerFactory emf
-            ) {
-        return new JpaTransactionManager(emf);
+    public JPAQueryFactory chatQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 }
